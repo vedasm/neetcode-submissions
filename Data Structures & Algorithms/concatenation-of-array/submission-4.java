7c282ac1ea3f1cbafebe83673fd59d3f10fcf1f5
Veda S M <129061546+vedasm@users.noreply.github.com>
2026-08-15 20:52:54 +0530

    Remove: concatenation-of-array - submission-4

--- Data Structures & Algorithms/concatenation-of-array/submission-4.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] getConcatenation(int[] nums) {
-        int[] ans = new int[nums.length*2];
-        for(int i = 0; i < nums.length; i++){
-            ans[i+nums.length] = ans[i] = nums[i]; 
-        }
-        return ans;
-    }
-}
